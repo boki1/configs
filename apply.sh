d690637a9fae71e2142bcc188fbcc606d9e68222
Kristiyan Stoimenov <kristoimenov@gmail.com>
2021-11-19 17:26:22 +0200

    Scripts: Add script for automatic updating of directories containing config files
    Signed-off-by: Kristiyan Stoimenov <kristoimenov@gmail.com>

---
+++ apply.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+CONFIG_DIR=~/.config
+
+#
+# Apply the changes made on the repo dir (this one)
+# with the files in CONFIG_DIR
+#
+
+cp zsh/zshrc 				$CONFIG_DIR/zsh
+cp alacritty/alacritty.yml 	$CONFIG_DIR/alacritty/
+cp nvim/init.vim 			$CONFIG_DIR/nvim/
+cp nvim/conf.vim 			$CONFIG_DIR/nvim/
+cp nvim/maps.vim 			$CONFIG_DIR/nvim/
+cp zathura/zathurarc 		$CONFIG_DIR/zathura/
+cp git/config 				$CONFIG_DIR/git
+cp tmux/tmux.conf 			$CONFIG_DIR/tmux/
+cp env/* 					$CONFIG_DIR/env/
---
+++ fetch.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+CONFIG_DIR=~/.config
+
+#
+# Fetches the changes made on the configuration
+# files in CONFIG_DIR inside the repo dir (this one)
+#
+
+cp $CONFIG_DIR/zsh/zshrc 				zsh/
+cp $CONFIG_DIR/alacritty/alacritty.yml 	alacritty/
+cp $CONFIG_DIR/nvim/init.vim 			nvim/
+cp $CONFIG_DIR/nvim/conf.vim 			nvim/
+cp $CONFIG_DIR/nvim/maps.vim 			nvim/
+cp $CONFIG_DIR/zathura/zathurarc 		zathura/
+cp $CONFIG_DIR/git/config 				git/
+cp $CONFIG_DIR/tmux/tmux.conf 			tmux/
+cp $CONFIG_DIR/env/* 					env/
+
