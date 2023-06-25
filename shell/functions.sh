f1211781b79328a570addb37076fd825581c9411
Kristiyan Stoimenov <kristoimenov@gmail.com>
2023-06-26 00:22:20 +0300

    Shell: Add *-format-all() function
    Signed-off-by: Kristiyan Stoimenov <kristoimenov@gmail.com>

--- shell/functions.sh
+++ shell/functions.sh
@@ -2,3 +2,13 @@ function previewmd() {
 	input_file=${1:-README.md}
 	pandoc -f markdown ${input_file} -t pdf | zathura -
 }
+
+function cmake-format-all() {
+	input_dirs=${1:-.}
+	find ${input_dirs} -name '*.cmake' -exec cmake-format -i {} \;
+}
+
+function clang-format-all() {
+	input_dirs=${1:-.}
+	find ${input_dirs} -type f -iname \*\.cpp -o -iname \*\.hpp -o -iname \*\.c -o -iname \*\.h -exec clang-format -i {} \;
+}
